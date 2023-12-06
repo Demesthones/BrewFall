367cd66081a0db592e904b800d800decb286806d
Teagan <demesthones@gmail.com>
2023-12-06 15:20:16 -0800

    Refactor messages, fix async -> promises

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,7 @@
 chrome.runtime.onInstalled.addListener((details) => {
+    let current_list = [];
     try{
-        let current_list = chrome.storage.local.get("global_list");
+        current_list = chrome.storage.local.get(["global_list"]);
     } catch (e){
         console.log("no storage. creating");
         chrome.storage.local.set({global_list:[]});
@@ -8,28 +9,24 @@ chrome.runtime.onInstalled.addListener((details) => {
 })
 
 chrome.runtime.onMessage.addListener((response, sender, sendReponse) => {
-    if (response.action === 'update_storage'){
-        chrome.storage.local.get("global_list", function(items){
-            if(items.global_list === undefined){
-                current_list = [];
-            } else {
-                let current_list = items.global_list;
+    let current_list = [];
+    try{
+        chrome.storage.local.get(["global_list"]).then((result) => {
+            if(result.global_list != undefined){
+                current_list = result.global_list;
             }
-            current_list.push(response.action_data);
+            if (response.action === 'update_storage'){
+                current_list.push(response.action_data);
+            } else if (response.action === 'clear_storage'){
+                current_list = []
+            } else if (response.action === 'decrease_storage'){
+                current_list.splice(current_list.indexOf(response.action_data),1);
+            } else if (response.action === 'remove_storage'){
+                current_list = result.global_list.filter(a => a !== response.action_data);
+            }
             chrome.storage.local.set({global_list:current_list});
         });
-    } else if (response.action === 'clear_storage'){
-        chrome.storage.local.set({global_list:[]});
-    } else if (response.action === 'decrease_storage'){
-        chrome.storage.local.get("global_list", function(items){
-            let current_list = items.global_list;
-            current_list.splice(current_list.indexOf(response.action_data),1);
-            chrome.storage.local.set({global_list:current_list});
-        });
-    } else if (response.action === 'remove_storage'){
-        chrome.storage.local.get("global_list", function(items){
-            let current_list = items.global_list.filter(a => a !== response.action_data);
-            chrome.storage.local.set({global_list:current_list});
-        });
+    } catch(e){
+        console.log(e);
     }
 });
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,19 +1,60 @@
 const scrollbox = document.getElementById("decklist-scrollbox");
 
-chrome.storage.onChanged.addListener((changes, namespace) => {
-    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+const addListener = function(){
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+        for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+            let target = scrollbox;
+            const template = document.querySelector("#row-item");
+            if (newValue === ""){
+                target.textContent = "";
+            } else {
+                target.textContent = "";
+                let current_list = newValue.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
+                for(const n in current_list){
+                    let clone = template.content.cloneNode(true);
+                    let cba = clone.querySelector(".card-box-amount");
+                    let cbn = clone.querySelector(".card-box-name");
+
+                    cba.textContent = `${current_list[n]} `;
+                    cbn.textContent = `${n}`;
+                    target.appendChild(clone);
+                };
+            }
+            let cnb = scrollbox.querySelectorAll("button");
+            for (let i of cnb){
+                i.addEventListener('click', function(){
+                    if(this.className === 'button-remove-amount'){
+                        chrome.runtime.sendMessage({action:'decrease_storage',action_data:this.parentElement.parentElement.querySelector(".card-box-name").textContent});
+                    }
+                    if(this.className === 'button-add-amount'){
+                        chrome.runtime.sendMessage({action:'update_storage',action_data:this.parentElement.parentElement.querySelector(".card-box-name").textContent});
+                    }
+                    if(this.className === 'button-delete-row'){
+                        chrome.runtime.sendMessage({action:'remove_storage',action_data:this.parentElement.parentElement.querySelector(".card-box-name").textContent});
+                    }
+                });
+            }
+        }
+    });
+}
+addListener();
+
+
+function updateScrollBox() {
+    chrome.storage.local.get(["global_list"]).then((result) => {
+        console.log(result.global_list);
         let target = scrollbox;
         const template = document.querySelector("#row-item");
-        if (newValue === ""){
+        if (result === ""){
             target.textContent = "";
         } else {
             target.textContent = "";
-            let current_list = newValue.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
+            let current_list = result.global_list.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
             for(const n in current_list){
                 let clone = template.content.cloneNode(true);
                 let cba = clone.querySelector(".card-box-amount");
                 let cbn = clone.querySelector(".card-box-name");
-
+    
                 cba.textContent = `${current_list[n]} `;
                 cbn.textContent = `${n}`;
                 target.appendChild(clone);
@@ -33,81 +74,30 @@ chrome.storage.onChanged.addListener((changes, namespace) => {
                 }
             });
         }
-    }
-});
-
-
-async function updateScrollBox() {
-    let content = await getGlobalList();
-    let target = scrollbox;
-    const template = document.querySelector("#row-item");
-    if (content === ""){
-        target.textContent = "";
-    } else if (content === undefined){
-        console.log("content undefined");
-    } else {
-        target.textContent = "";
-        let current_list = content.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
-        for(const n in current_list){
-            let clone = template.content.cloneNode(true);
-            let cba = clone.querySelector(".card-box-amount");
-            let cbn = clone.querySelector(".card-box-name");
-
-            cba.textContent = `${current_list[n]} `;
-            cbn.textContent = `${n}`;
-            target.appendChild(clone);
-        };
-    }
-    let cnb = scrollbox.querySelectorAll("button");
-    for (let i of cnb){
-        i.addEventListener('click', function(){
-            if(this.className === 'button-remove-amount'){
-                chrome.runtime.sendMessage({action:'decrease_storage',action_data:this.parentElement.parentElement.querySelector(".card-box-name").textContent});
-            }
-            if(this.className === 'button-add-amount'){
-                chrome.runtime.sendMessage({action:'update_storage',action_data:this.parentElement.parentElement.querySelector(".card-box-name").textContent});
-            }
-            if(this.className === 'button-delete-row'){
-                chrome.runtime.sendMessage({action:'remove_storage',action_data:this.parentElement.parentElement.querySelector(".card-box-name").textContent});
-            }
-        });
-    }
-}
-
-function getGlobalList() {
-    let gl = "";
-    try{
-        chrome.storage.local.get("global_list", function(items){
-            gl = items.global_list;
-        });
-    } catch (error){
-        console.log("error", error);
-    }
-    return new Promise((resolve, reject) => {
-        resolve(gl);
     });
 }
 
 const btns = document.querySelectorAll('button');
 for (let i of btns){
-    i.addEventListener('click', async function(){
+    i.addEventListener('click', function(){
         if(this.id === 'copy'){
-            let content = await getGlobalList();
-            content = content.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
-            let cb = "";
-            for(const n in content){
-                cb += `${content[n]}  ${n}\n`;
-            }
-            console.log(cb);
-            navigator.clipboard.writeText(cb);
+            chrome.storage.local.get(["global_list"]).then((result) => {
+                //console.log(content);
+                let content = result.global_list.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
+                let cb = "";
+                for(const n in content){
+                    cb += `${content[n]}  ${n}\n`;
+                }
+                //console.log(cb);
+                navigator.clipboard.writeText(cb);
+            });
         }
         if(this.id === 'clear'){
-            Promise.resolve(chrome.runtime.sendMessage({action:'clear_storage'})).then(() => undefined);
+            chrome.runtime.sendMessage({action:'clear_storage'});
         }
-        updateScrollBox();
     });
 }
 
-window.onload = function() {
+window.onload = function(){
     updateScrollBox();
 }
